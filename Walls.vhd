c63331c72b147ddab675fcb05ca3c2bfa05dcf75
Ricardo Piro-Rael <fdisk@fdisk.co>
2014-11-12 20:07:07 -0700

    We picked initial T

--- Walls.vhd
+++ Walls.vhd
@@ -75,25 +75,28 @@ Wall_on <= Wall;
 ----- Top Wall ---- 
 Wall_T_On <= '1' when ((Y < "0000110010") and (video_on='1')) else '0';
 ---My splat on the screen---
---Wall_I_on <= '1' when ((X > "0001100100") and (Y > "0001100100") and (X < "0011001000") and (Y < "0011001000") and (video_on='1')) else '0';
+--Top of the T
+Wall_I_on <= '1' when ((X > "0001100100") and (Y > "0001100100") and (X < "0011001000") and (Y < "0001111100") and (video_on='1')) else
+             '1' when ((X > "0010001001") and (Y > "0001111011") and (X < "0010100010") and (Y < "0011001000") and (video_on='1')) else
+				 '0';
 
-	 sq_d_on <= '1' when ((X > "0001100100") and (Y > "0001100100")and (X < "0001110011") and (Y < "0001110011")
-								   and (video_on='1')) else 
-					  '0';
+--	 sq_d_on <= '1' when ((X > "0001100100") and (Y > "0001100100")and (X < "0001110011") and (Y < "0001110011")
+								   --and (video_on='1')) else 
+					  --'0';
   -- map scan coord to ROM addr/col -- use low order three
 	-- bits of pixel and ball positions.
 	-- ROM row
-	rom_row <= pix_y(3 downto 0) - dbottom(3 downto 0);
+	--rom_row <= pix_y(3 downto 0) - dbottom(3 downto 0);
 	-- ROM column
-	rom_col <= pix_x(3 downto 0) - dbottom(3 downto 0);
+	--rom_col <= pix_x(3 downto 0) - dbottom(3 downto 0);
 	-- Get row data
-	rom_data <= D_ROM(to_integer(rom_row));
+	--rom_data <= D_ROM(to_integer(rom_row));
 	-- Get column bit
-	rom_bit <= rom_data(to_integer(rom_col));
+	--rom_bit <= rom_data(to_integer(rom_col));
 	
 	-- Turn ball on only if within square and ROM bit is 1.
-	Wall_I_On <= '1' when (sq_d_on = '1') and (rom_bit = '1') else 
-					  '0';
+	--Wall_I_On <= '1' when (sq_d_on = '1') and (rom_bit = '1') else 
+	--				  '0';
 	--ball_on <= rd_ball_on;
 					 
 ----- Bottom Wall ---- 
